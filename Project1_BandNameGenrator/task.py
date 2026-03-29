96a9a862576da9f15451bca1c3333604433b2792
Krishna-verma97 <kv945164@gmail.com>
2026-03-29 21:16:49 +0530

    First Project Basic concept of python

---
+++ Project1_BandNameGenrator/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the world of Python!!!")
+UserCity = input("Enter your city where you grew up = \n")
+UserPetName = input("Enter your pet name = \n ")
+print("Yupp!!!, We generated your's Band name: \n "+ UserCity + UserPetName)
